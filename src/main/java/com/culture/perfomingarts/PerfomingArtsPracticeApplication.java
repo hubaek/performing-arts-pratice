7e6e5fcb77b2b583ba56efeace9a4e8df3fc3586
hubaek <zjfl3122@naver.com>
2025-01-27 22:45:32 +0900

    init project

---
+++ src/main/java/com/culture/perfomingarts/PerfomingArtsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.culture.perfomingarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfomingArtsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfomingArtsPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/culture/perfomingarts/PerfomingArtsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.culture.perfomingarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfomingArtsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
